db1a2751d2210ae0a80294a4a20430cb4337d20a
fjquen <fjquen@gmail.com>
2024-10-17 22:19:49 +0200

    mise en place model food

--- model/Food.rb
+++ model/Food.rb
@@ -1,2 +1,25 @@
 class Food
+    def initialize()
+        @foodPosition = []
+        @listObservers = []
+    end
+
+
+    def subscribe(model)
+        @listObservers.push(model)
+    end
+
+    def descscribe(model)
+        @listObservers.delete(model)
+    end
+
+    def notify
+        @listObservers.each do |observer|
+          observer.updatePosition(@foodPosition)
+        end
+    end
+
+    def foodPosition
+        return @foodPosition
+    end
 end
